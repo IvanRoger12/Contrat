a1e561431707a5108cff9aa68d630cf169d97d81
Ivan NFINDA <151882341+IvanRoger12@users.noreply.github.com>
2025-08-18 21:10:10 +0200

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import ContraScope from "./ContraScope"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ContraScope />
+  </React.StrictMode>
+)
