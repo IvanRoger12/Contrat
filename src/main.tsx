ab56e01db5692e241953e66c2e748390be6219b0
Ivan NFINDA <151882341+IvanRoger12@users.noreply.github.com>
2025-08-19 00:33:20 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.css"
-import ContraScope from "./ContraScope"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ContraScope from "./ContraScope";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <ContraScope />
   </React.StrictMode>
-)
+);
